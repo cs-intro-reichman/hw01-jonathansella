f26860b0dba8d147450485a4d6c199cf4facab2b
unknown <jonathansella1@gmail.com>
2023-12-21 15:30:00 +0200

    homework 1

--- Triangle.java
+++ Triangle.java
@@ -3,7 +3,6 @@ class Triangle {
 		int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[1]);	
 		int c = Integer.parseInt(args[2]);
-	    System.out.println(a + "," + b + "," + c + ":");
-		System.out.println(a + b > c);
+	    System.out.println(a + ", " + b + ", " + c + ": " + (a + b > c));
 		}
 	}
